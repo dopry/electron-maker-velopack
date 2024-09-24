0fd06b1b7e4af47ba343a31c3fd16032e3a16067
Edwin Steiner <edwin.steiner@gmx.net>
2024-09-24 19:17:38 +0200

    fill in isSupportedOnCurrentPlatform()

--- src/MakerVelopack.ts
+++ src/MakerVelopack.ts
@@ -11,7 +11,15 @@ export default class MakerVelopack extends MakerBase<MakerVelopackConfig> {
     defaultPlatforms: ForgePlatform[] = ['win32'];
 
     isSupportedOnCurrentPlatform(): boolean {
-        return 
+        if (process.platform !== 'win32') {
+            console.log("electron-maker-velopack is currently only supported on the win32 platform.");
+            return false;
+        }
+        if (!this.isInstalled("velopack")) {
+            console.log("Package 'velopack' is not installed, therefore electron-maker-velopack is not supported.");
+            return false;
+        }
+        return true;
     }
 
     async make({ dir, makeDir, targetArch, packageJSON, appName, forgeConfig }: MakerOptions): Promise<string[]> {
